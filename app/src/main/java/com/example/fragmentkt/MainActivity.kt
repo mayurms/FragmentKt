e062190dcb1214068bb87ecc51e640b8869c509d
mayur <mayurms5060@gmail.com>
2023-09-14 13:51:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fragmentkt/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.fragmentkt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.fragmentkt.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonFragment1.setOnClickListener{
+            goToFragment(Fragment1())
+        }
+
+        binding.buttonFragment2.setOnClickListener{
+            goToFragment(Fragment2())
+        }
+
+    }
+    private fun goToFragment(fragment: Fragment){
+        fragmentManager =supportFragmentManager
+        fragmentManager.beginTransaction().replace(R.id.fragment_container,fragment).commit()
+    }
+}
